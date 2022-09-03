994eebcd009bb0f8a98de8820c8fb8148822b2b4
Gonzalo Arrayaran <gonzalo.arrayaran@gmail.com>
2022-09-03 16:28:32 -0300

    Creado el proyecto inicial

---
+++ proyecto1/sintaxis-variables-palabras/index.js
@@ -0,0 +1,14 @@
+const nombre = [
+    "Javier",
+    24,
+    "True",
+    new Date(1997 - 1, 12, 23),
+    {
+        titulo: "50 sombras de grey",
+        autor: "Arnol",
+        fecha: new Date(2010 -1, 1, 21),
+        url: "http://www.arnoldfit.com.ar"
+    }
+];
+
+console.log(nombre);
