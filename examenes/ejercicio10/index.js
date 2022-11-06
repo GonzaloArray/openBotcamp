64fa75720e1dd793a138ba55340e302659b23dee
GonzaloArray <gonzalo.arrayaran@gmail.com>
2022-11-06 19:06:49 -0300

    agregando ejercicios resueltos test 8

--- examenes/ejercicio10/index.js
+++ examenes/ejercicio10/index.js
@@ -11,3 +11,45 @@ function saludar(nombre, apellido, dni) {
 }
 
 console.log(saludar("", "Ceo de tortillalan", "123123"))
+
+// Carga y sobrecarga de funciones
+// Es cuando la funcion es llamada dentro de otra funcion
+// Ejemplo
+function hola() {
+    saludar();
+}
+
+hola();
+
+// Promesas
+
+const nuevaPromesa = new Promise((resolve, reject) => {
+    const i = Math.floor(Math.random() * 2);
+
+    if (i !== 0) {
+        return resolve()
+    }else{
+        return reject();
+    }
+})
+
+nuevaPromesa
+    .then(() => console.log("Se ha ejecutado correctamente"))
+    .catch(() => console.log("Ha ocurrido un problemon"))
+    .finally(() => console.log("Yo me ejecuto siempre"))
+
+// Funciones generadoras
+function* generaId() {
+    let id = 0;
+    while(true){
+        id++;
+        if (id > 10) {
+            return
+        }
+        yield id; /* Esperando hasta que se vuelva a llamar la funcion generadora se queda esperando hasta que la llamemos devuelta*/
+    }
+}
+
+const gen = generaId();
+
+console.log(gen.next()); /* Para obtener un valor se utiliza next() */
